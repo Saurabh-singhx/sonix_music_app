493d4b8d009c2db388232fdd94e0aff4e9403761
saurabh kumar <saurabh4442kumar@gmail.com>
2026-01-26 16:36:43 +0530

    playlist-route

--- server/src/modules/user/user.routes.ts
+++ server/src/modules/user/user.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../../middleware/auth.middleware.js";
-import { getAllPlaylist, getAllSongs, getMyPlaylists, getPublicPlaylists } from "./user.controllers.js";
+import { getAllPlaylist, getAllSongs, getMyPlaylists, getPlaylistsSongs, getPublicPlaylists } from "./user.controllers.js";
 import { createArtist } from "../admin/admin.controller.js";
 
 const router = express.Router();
@@ -15,7 +15,7 @@ router.post("createplaylist",protectRoute,createArtist);
 router.get("/getmyplaylists",protectRoute,getMyPlaylists);
 router.get("/getpublicplaylists",protectRoute,getPublicPlaylists);
 router.get("/getallplaylists",protectRoute,getAllPlaylist)
-
+router.get("/getplaylistsongs/:playlistId",protectRoute,getPlaylistsSongs)
 
 
 export default router;

16857766b3326242022d1b99df7404b9663bc99c
saurabh kumar <saurabh4442kumar@gmail.com>
2026-02-01 23:14:15 +0530

    auth routes rate-limter added

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -6,20 +6,18 @@ import { checkLoginValidation } from './validators/login.validators.js';
 import passport from 'passport';
 import { checkEmailValidation } from './validators/email.validators.js';
 import { checkAuth, login, logout, otpSend, signup } from './auth.controller.js';
-import { loginLimiter, otpLimiter, signupLimiter } from '../../middleware/ratelimit.js';
+import { checkAuthLimiter, loginLimiter, logOutLimiter, otpLimiter, signupLimiter } from '../../middleware/ratelimit.js';
 
 
 const router = express.Router();
 
 
-// rate limiting is not added fix ----->--->
-
 router.post("/otp",otpLimiter, checkEmailValidation,validate, otpSend);
 router.post("/signup",signupLimiter, checkSignupValidation,validate,signup);
 router.post("/login",loginLimiter, checkLoginValidation,validate,login);
-router.post("/logout", logout);
+router.post("/logout",logOutLimiter, logout);
 // router.get("/google",passport.authenticate("google", { scope: ["profile", "email"],session: false }));
 // router.get("/google/callback",passport.authenticate("google", { failureMessage: true, session: false }),googleAuth);
-router.get("/checkauth",protectRoute,checkAuth);
+router.get("/checkauth",checkAuthLimiter, protectRoute,checkAuth);
 
 export default router;

f98b555e4921a910c1c62e12d995852325a2c8fe
saurabh kumar <saurabh4442kumar@gmail.com>
2026-02-01 13:58:16 +0530

    auth rate-limit improved

--- server/src/middleware/ratelimit.ts
+++ server/src/middleware/ratelimit.ts
@@ -2,6 +2,9 @@ import {rateLimit} from "express-rate-limit";
 import RedisStore from "rate-limit-redis";
 import redisClient from "../config/redis.js";
 
+
+
+// auth routes ==----==>
 /**
  * LOGIN rate limit
  * 5 attempts / 15 minutes
@@ -11,13 +14,20 @@ export const loginLimiter = rateLimit({
     max: 5,
     standardHeaders: true,
     legacyHeaders: false,
+
+    keyGenerator: (req) => `${req.body?.email ?? "unknown"}`,
+
+
     store: new RedisStore({
-    sendCommand: (...args) => redisClient.sendCommand(args),
-    prefix: "rl:login:", 
-  }),
-    message: {
-        message: "Too many login attempts. Try again later.",
-    },
+      sendCommand: (...args) => redisClient.sendCommand(args),
+      prefix: "rl:login:", 
+    }),
+
+    handler: (_req, res) => {
+      res.status(429).json({
+        message: "Too many login requests. Try again after 15 minutes."
+      });
+    }
 });
 
 /**
@@ -29,13 +39,18 @@ export const signupLimiter = rateLimit({
     max: 10, 
     standardHeaders: true,
     legacyHeaders: false,
+
+    keyGenerator: (req) => `${req.body?.email ?? "unknown"}`,
+
     store: new RedisStore({
-    sendCommand: (...args) => redisClient.sendCommand(args),
-    prefix: "rl:signup:", 
-  }),
-    message: {
-        message: "Too many signup attempts. Try again later.",
-    },
+      sendCommand: (...args) => redisClient.sendCommand(args),
+      prefix: "rl:signup:", 
+    }),
+    handler: (_req, res) => {
+    res.status(429).json({
+      message: "Too many signup requests. Try again after 1 hour."
+    });
+  },
 });
 
 // otp rate limit  5 request / 10 minutes
@@ -45,13 +60,64 @@ export const otpLimiter = rateLimit({
   max:5,
   standardHeaders:true,
   legacyHeaders:false,
+  
+  keyGenerator: (req) => `${req.body?.email ?? "unknown"}`,
+
   store: new RedisStore({
-    sendCommand :(...args) => redisClient.sendCommand(args),
+    sendCommand: (...args) => redisClient.sendCommand(args),
     prefix: "rl:otp",
   }),
-  message:{
-    message:"Too many attempts. "
-  }
+
+  handler: (_req, res) => {
+    res.status(429).json({
+      message: "Too many otp requests. Try again after 10 minutes."
+    });
+  },
 });
+
+
+export const logOutLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 100,
+
+  standardHeaders: true,
+  legacyHeaders: false,
+
+  keyGenerator: (req) => `${req.ip}`,
+
+  store: new RedisStore({
+    prefix: "rl:logout:",
+    sendCommand: (...args) => redisClient.sendCommand(args),
+  }),
+
+  handler: (_req, res) => {
+    res.status(429).json({
+      message: "Too many logout requests. Try again later."
+    });
+  },
+});
+
+
+export const checkAuthLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 30,
+  standardHeaders: true,
+  legacyHeaders: false,
+
+  keyGenerator: (req) =>
+    `${req.ip}:${req.cookies?.jwtauth ?? "anon"}`,
+
+  store: new RedisStore({
+    prefix: "rl:checkAuth:",
+    sendCommand: (...args) => redisClient.sendCommand(args),
+  }),
+
+  handler: (_req, res) => {
+    res.status(429).json({
+      message: "Too many checkAuth attempts. Try again later."
+    });
+  },
+});
+
 
 

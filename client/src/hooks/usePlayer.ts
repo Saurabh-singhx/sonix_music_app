dead9ab044d2b0c0d38433d4e0f4cee21d6ec3d0
saurabh kumar <saurabh4442kumar@gmail.com>
2026-02-16 06:28:11 +0530

    fixed player-progress

--- client/src/hooks/usePlayer.ts
+++ client/src/hooks/usePlayer.ts
@@ -1,15 +1,15 @@
 // hooks/useGlobalPlayer.ts
 import { usePlayerStore } from "@/store/player/player.store";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAudioPlayer } from "react-use-audio-player";
 
 export function useGlobalPlayer() {
   const { currentTrack, next, prev } = usePlayerStore();
   const player = useAudioPlayer();
+  const [progress, setProgress] = useState(0)
 
-  const [position, setPosition] = useState(0);
-  const rafRef = useRef<number | null>(null);
-
+  const [currentTime, setCurrentTime] = useState('0:00');
+  const [position, setPosition] = useState(0)
   // Load song
   useEffect(() => {
     if (!currentTrack?.song_url) return;
@@ -23,28 +23,38 @@ export function useGlobalPlayer() {
     player.play()
   }, [currentTrack?.song_url, next]);
 
-  // Track playback position
-  useEffect(() => {
-    const update = () => {
+   useEffect(() => {
+    const interval = setInterval(() => {
       if (player.isPlaying) {
         setPosition(player.getPosition());
       }
-      rafRef.current = requestAnimationFrame(update);
-    };
+    }, 200); 
 
-    rafRef.current = requestAnimationFrame(update);
+    return () => clearInterval(interval);
+  }, [player]);
 
-    return () => {
-      if (rafRef.current) cancelAnimationFrame(rafRef.current);
-    };
-  }, [player.isPlaying]);
+  useEffect(() => {
+      if (!currentTrack || !player.duration) return;
+  
+      const progressValue = (position / player.duration) * 100;
+      setProgress(progressValue);
+  
+      const mins = Math.floor(position / 60);
+      const secs = Math.floor(position % 60);
+  
+      setCurrentTime(`${mins}:${secs.toString().padStart(2, '0')}`);
+  
+    }, [position, player.duration, currentTrack]);
+
+  
 
   return {
     /* state */
     currentTrack,
     playing: player.isPlaying,
     duration: player.duration,
-    position, // ✅ now reactive
+    progress,
+    currentTime,
 
     /* controls */
     play: player.play,

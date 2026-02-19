1cefb1d7c5c1ffaa7a92e2923e81596863a3aed8
saurabh kumar <saurabh4442kumar@gmail.com>
2026-02-20 03:26:48 +0530

    jwt fix same-site

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ const PORT: number = Number(process.env.PORT) || 4000;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cookieParser());
-app.set('trust proxy', 1); // '1' is the number of proxies between the user and the server
+app.set('trust proxy', 1);
 
 app.use(
   cors({
--- server/src/lib/jwt.ts
+++ server/src/lib/jwt.ts
@@ -2,10 +2,6 @@ import jwt from "jsonwebtoken";
 import { Response } from "express";
 import { JwtPayload } from "jsonwebtoken";
 
-interface AuthPayload extends JwtPayload {
-  userId: string;
-}
-
 export const generateToken = (userId: string, res: Response): string => {
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined in .env");
@@ -16,8 +12,8 @@ export const generateToken = (userId: string, res: Response): string => {
   res.cookie("jwtauth", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    sameSite: "strict",
     secure: process.env.NODE_ENV !== "development",
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
   });
 
   return token;
